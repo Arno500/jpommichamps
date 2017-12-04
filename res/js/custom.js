5120bda70405f80d89b79cb0914f404e9df441ab
Arno DUBOIS <arno.du@orange.fr>
2017-12-04 14:40:34 +0100

    First commit

---
+++ res/js/custom.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+    'use strict';
+    $('.parallax').parallax();
+});
+
+//From https://stackoverflow.com/a/18365991
+
+function smoothScrollingTo(target) {
+    'use strict';
+    $('html,body').animate({
+        scrollTop: $(target).offset().top
+    }, 1000, 'easeOutExpo');
+}
+$('a[href*=\\#]').on('click', function (event) {
+    'use strict';
+    event.preventDefault();
+    smoothScrollingTo(this.hash);
+});
+
+$(document).ready(function () {
+    'use strict';
+    smoothScrollingTo(location.hash);
+});
+
+$(document).idle({
+    onIdle: function () {
+        'use strict';
+        smoothScrollingTo($("#front"));
+    },
+    idle: 120000
+});
+
+$(document).scroll(function () {
+    'use strict';
+    $('.section').each(function () {
+        var post = $(this);
+        var navitem = $('a[href="#' + post.attr("id") + '"]');
+        var position = post.position().top - $(window).scrollTop();
+
+        if (position <= 50 && position + post.height() >= 20) {
+            navitem.addClass('active');
+        } else {
+            navitem.removeClass('active');
+        }
+    });
+});
